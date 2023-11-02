12da4a8e9f82a53556f206fc3b781210883c0478
leon <jonny.l.nascimento@gmail.com>
2023-11-02 16:08:31 +0000

    utils first update

--- utils.py
+++ utils.py
@@ -1,1 +1,23 @@
 # YOUR FUNCTIONS HERE
+
+from redis import redis
+from redis.commands.search.field import TextField
+from redis.commands.search.field import NumericField
+
+
+client = redis.Redis(decode_responses=True, protocol=3)
+
+
+# = redis.Redis()
+index_name = "my_index"
+schema = (
+    TextField("addType", weight=5.0),
+    TextField("assetType"),
+    TextField("county"),
+    NumericField("price"),
+    NumericField("grossArea"),
+    NumericField("numberOfRooms"),
+    NumericField("enteredMarket"),
+)
+client.ft(index_name).create_index(schema)
+print(r.ft(index_name).info())
