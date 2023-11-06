58dd088cbf7c4d516bea68e89d44cc3f9c35abf0
leon <jonny.l.nascimento@gmail.com>
2023-11-06 15:02:02 +0000

    Started API, addType, assetType, county, rooms search

--- api.py
+++ api.py
@@ -10,8 +10,6 @@ import json
 import redis
 
 
-app = FastAPI()
-
 class Property(BaseModel):
     addType: str
     assetType: str
@@ -41,7 +39,61 @@ class Property(BaseModel):
     }
 
 
+#connect to redis
+client = redis.Redis(decode_responses=True, protocol=3)
+
+#start API application
+app = FastAPI()
+
 @app.post("/metrics")
+def property_in_type(property_id: int, property: Property):
+
+
+    addType_search_full = property.addType.split(",")
+    assetType_search_full = property.assetType.split(",")
+    county_search_full = property.county.split(",")
+    numberOfRooms_search_full = property.numberOfRooms.split(",")
+    
+
+    addType_search_clause = ""
+    assetType_search_clause = ""
+
+    for addType_search in addType_search_full:
+        addType_search_clause = f"{addType_search_clause} @:addType {{{addType_search}}}"
+
+    for assetType_search in assetType_search_full:
+        assetType_search_clause = f"{assetType_search_clause} @:assetype {{{assetType_search}}}"
+
+    for county_search in county_search_full:
+        county_search_clause = f"{county_search_clause} @:county {{{county_search}}}"
+
+    for county_search in county_search_full:
+        county_search_clause = f"{county_search_clause} @:county {{{county_search}}}"
+
+
+    addType_search_result = client.ft("property_index_json").search(
+        Query()
+    )
+
 def metrics():
 
     return 
+
+
+#search by range of values: price, grossArea, enteredMarket; represented by a dictionary [min max]
+##
+res_price = client.ft("property_index_json").search(
+    Query("@price: [min_price max_price]")
+)
+
+res_grossArea = client.ft("property_index_json").search(
+    Query("@grossArea: [min_area max_area]")
+)
+
+res_enteredMarket = client.ft("property_index_json").search(
+    Query("@enteredMarket: [min_market max_market]")
+)
+##
+
+
+#search by single or multiple values
