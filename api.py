79d969f7b72e20dc9f8a11274aff1b84e23700d6
leon <jonny.l.nascimento@gmail.com>
2023-11-06 14:16:08 +0000

    Started API, imported base model from declare request example data

--- api.py
+++ api.py
@@ -1,1 +1,47 @@
 # YOUR API HERE
+from re import S
+from fastAPI import FastAPI
+from pydantic import BaseModel
+from redis.commands.search.aggregation import AggregateRequest
+from redis.commands.search.query import Query
+from redis.exceptions import ResponseError
+
+import json
+import redis
+
+
+app = FastAPI()
+
+class Property(BaseModel):
+    addType: str
+    assetType: str
+    county: str
+    price: float
+    grossArea: float
+    numberOfRooms: int
+    enteredMarket: float
+
+    model_config = {
+        "json_schema": {
+            "examples" : [
+                {
+                    "addType": "sell",
+                    "assetType": ["apartment", "house"],
+                    "grossArea": {
+                        "min": 50,
+                        "max": 100
+                                },
+                    "enteredMarket": {
+                    "min": "2023-10-20"
+                    }
+
+                }
+            ]  
+        }
+    }
+
+
+@app.post("/metrics")
+def metrics():
+
+    return 
